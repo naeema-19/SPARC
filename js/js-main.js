a4091f02d81bed8156321c9e10ac5f9fd14d5773
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2022-09-21 22:47:21 +0530

    Update js-main.js

--- js/js-main.js
+++ js/js-main.js
@@ -38,7 +38,30 @@
       }
     }
   });
-
+new Swiper('.testimonialss-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+      
+      1200: {
+        slidesPerView: 2,
+      }
+    }
+  });
 new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -83,7 +106,7 @@ new Swiper('.posters-slider', {
       },
       
       1200: {
-        slidesPerView: 3,
+        slidesPerView: 2,
       }
     }
   });
